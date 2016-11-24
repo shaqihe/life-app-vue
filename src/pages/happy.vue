327b2a6302318cda0d5c4a66e2ba71cbb54e6008
shaqihe <330630770@qq.com>
2016-11-24 12:08:49 +0800

    @misc 增加loading

--- src/pages/happy.vue
+++ src/pages/happy.vue
@@ -1,25 +1,28 @@
 <template>
     <div class="page-happy">
         <page-header :pageType="pageType" />
+        <loading v-show="loading"/>
         <div>
         开心一刻网页
-        </div>3e21eq
+        </div>
     </div>
 </template>
 
 <script>
 import pageHeader from '../components/Header'
+import Loading from '../components/Loading'
 import {PAGE_TYPE} from '../common/constant/constant';
 
 export default {
     name: 'happy',
     data () {
         return {
-            pageType: PAGE_TYPE.HAPPY_PAGE
+            pageType: PAGE_TYPE.HAPPY_PAGE,
+            loading: false
         }
     },
     components: {
-        pageHeader
+        pageHeader,Loading
     }
 }
 </script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,21 +2,19 @@
     <!-- 全局header -->
     <div>
         <img class="index" src="../assets/images/5.jpg">
-        <div class="spinner">
-              <div class="rect1"></div>
-              <div class="rect2"></div>
-              <div class="rect3"></div>
-              <div class="rect4"></div>
-              <div class="rect5"></div>
-        </div>
+        <loading></loading>
     </div>
 </template>
 <script>
+    import Loading from '../components/Loading'
     export default {
         created (){
             setTimeout(() => {
                 this.$router.push({ name: 'happy'});
             },2000);
+        },
+        components: {
+            Loading
         }
     }
 </script>
@@ -25,62 +23,6 @@
         width: 100%;
         background-color: #fff;
         heigth: 100%;
-    }
-    .spinner {
-        margin: 50px auto;
-        width: 50px;
-        height: 60px;
-        text-align: center;
-        font-size: 10px;
-    }
-
-    .spinner > div {
-        background-color: #7e7e85;
-        margin-top: -200px;
-        height: 100%;
-        width: 6px;
-        display: inline-block;
-        -webkit-animation: stretchdelay 1.2s infinite ease-in-out;
-        animation: stretchdelay 1.2s infinite ease-in-out;
-    }
-
-    .spinner .rect2 {
-        -webkit-animation-delay: -1.1s;
-        animation-delay: -1.1s;
-    }
-    .spinner .rect3 {
-        -webkit-animation-delay: -1.0s;
-        animation-delay: -1.0s;
-    }
-
-    .spinner .rect4 {
-        -webkit-animation-delay: -0.9s;
-        animation-delay: -0.9s;
-    }
-
-    .spinner .rect5 {
-        -webkit-animation-delay: -0.8s;
-        animation-delay: -0.8s;
-    }
-
-    @-webkit-keyframes stretchdelay {
-        0%, 40%, 100% {
-            -webkit-transform: scaleY(0.4)
-        }
-        20% {
-            -webkit-transform: scaleY(1.0)
-        }
-    }
-
-    @keyframes stretchdelay {
-        0%, 40%, 100% {
-            transform: scaleY(0.4);
-            -webkit-transform: scaleY(0.4);
-       }
-        20% {
-            transform: scaleY(1.0);
-           -webkit-transform: scaleY(1.0);
-        }
     }
 </style>
 
